7c1c375f16432bd544dda718ab796be0a790b56e
Rodrigo Ascencio <yiyoascen@gmail.com>
2020-01-25 17:38:25 -0600

    classifies a triangle

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,13 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == b && a == c
+    :equilateral
+  elsif a == b || a == c || c == b
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
